ef5970975a1998093783b09a1557bd3f74cccf16
wangxian <xian366@126.com>
2013-08-14 18:12:33 +0800

    create && edit && del && list

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,81 @@
 'use strict';
 
 /* Controllers */
+// function IndexCtrl() {
+// }
+
+// function EditCtrl() {
+// }
+
+var list = [
+  {
+    "id"      : 1,
+    "title"   : "安装mongoskin模块，npm install mongoskin",
+    "date"    : 1286461782373,
+    "finished": 0
+  },
+  {
+    "id"      : 2,
+    "title"   : "测试本地条已完成!",
+    "date"    : 1276461782373,
+    "finished": 1
+  }
+];
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('ListCtrl', ['$scope', '$location', function($scope, $location) {
 
+    $scope.list = list
+    $scope.itemClass = function (finished) {
+      // console.log("new:", finished)
+      return finished ? "del" : "new"
+    }
+
+    $scope.itemFinished = function (item) {
+      // console.log(item)
+      item.finished = 1
+    }
+
+    $scope.itemRenew = function(item) {
+      item.finished = 0;
+    }
+
+    $scope.itemDelete = function(item) {
+      for(var i=0, len=$scope.list.length;i<len;i++) {
+        if(item === $scope.list[i]) {
+          $scope.list.splice(i, 1);
+        }
+      }
+    }
+
+    $scope.newTodo = function(e) {
+      var data = {}
+      data.id = this.list.length+1;
+      data.title = e.target.value;
+      data.finished = 0
+      data.date = (new Date()).getTime()
+
+      this.list.push(data)
+    }
+
+    $scope.gotoEdit = function(item){
+      $location.url("/edit/"+ item.id)
+    }
   }])
-  .controller('MyCtrl2', [function() {
+
+
+  .controller('EditCtrl', ['$scope', '$routeParams', '$location', function($scope, $routeParams, $location) {
+    // console.log($routeParams)
+    // console.log(list)
+    var _data = list[ $routeParams.id - 1 ];
+    $scope.title = _data.title;
+
+    $scope.itemUpdate = function(e) {
+      _data.title = $scope.title;
+      $location.url("/list")
+    }
 
   }]);
+
+
+
--- app/js/directives.js
+++ app/js/directives.js
@@ -8,4 +8,26 @@ angular.module('myApp.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
+  }])
+
+  .directive('ngEnter', ['version', function(version) {
+    return function(scope, elm, attrs) {
+      // console.log(arguments)
+      elm.bind("keydown", function ($event) {
+        // console.log($event)
+        // console.log(attrs.ngEnter)
+
+        if($event.which === 13) {
+
+          // set event scope
+          scope.$event = $event
+          scope.$apply(function (){
+            scope.$eval(attrs.ngEnter);
+          });
+          elm.val("")
+          // $event.preventDefault();
+
+        }
+      });
+    };
   }]);
